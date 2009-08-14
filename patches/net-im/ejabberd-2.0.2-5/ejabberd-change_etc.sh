02b104ac40d2f4bf023a0c6d2ea2f8742449c34e
mhiretskiy <mhiretskiy@c91db197-33c1-4113-bf15-f8a5c547ca64>
2009-08-14 15:04:08 +0000

    git-svn-id: http://svn.calculate.ru/overlay@2450 c91db197-33c1-4113-bf15-f8a5c547ca64

--- patches/net-im/ejabberd-2.0.2-5/ejabberd-change_etc.sh
+++
@@ -1,7 +0,0 @@
-# changing /etc/init.d and /etc/conf.d
-
-post_src_install() {
-	einfo "Changing ejabberd initd and confd"
-	newinitd "${BASH_SOURCE/ejabberd-change_etc.sh}/${PN}-2.initd" ${PN} || die
-	newconfd "${BASH_SOURCE/ejabberd-change_etc.sh}/${PN}-2.confd" ${PN} || die
-}
--- patches/sys-kernel/gentoo-sources-kernelname.sh
+++
@@ -1,6 +0,0 @@
-# Changing extraversion of kernel
-
-if [[ ${EBUILD_PHASE} == compile ]]; then
-	einfo "Changing kernel extraversion"
-	sed -ri 's/^(EXTRAVERSION\s+=\s+)-gentoo-r([0-9]+)/\1.\2-calculate/' ${S}/Makefile 
-fi
