bdf26fa5e103840acc498d8e7d4abd416d3dd08d
Mike Hiretsky <mh@calculate.ru>
2010-01-22 19:13:43 +0300

    Fix layman update

---
+++ profiles/patches/app-portage/layman-1.3-9-update.sh
@@ -0,0 +1,17 @@
+
+if [[ ${EBUILD_PHASE} == postinst ]]
+then
+	if [[ ! -L ${ROOT}/usr/local/portage/layman ]] &&
+	   [[ -e ${ROOT}/usr/local/portage/layman ]]
+	then
+		ebegin "Move overlays"
+		local res=0
+		sed -ri 's|/usr/local/portage/layman|/var/lib/layman|' ${ROOT}/usr/local/portage/layman/make.conf || res=1
+		mv ${ROOT}/usr/local/portage/layman/* ${ROOT}/var/lib/layman/ || res=1
+		sed -ri 's|^(storage\s*:\s*)\S+$|\1/var/lib/layman|' ${ROOT}/etc/layman/layman.cfg || res=1
+		sed -ri 's|^(overlays\s*:\s*)\S+$|\1http://www.gentoo.org/proj/en/overlays/repositories.xml|' ${ROOT}/etc/layman/layman.cfg || res=1
+		rm -rf ${ROOT}/usr/local/portage/layman || res=1
+		ln -s /var/lib/layman ${ROOT}/usr/local/portage/layman || res=1
+		eend $res
+	fi
+fi
