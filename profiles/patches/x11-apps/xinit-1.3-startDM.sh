0a5dc8cc40b98e78906fd1455592b8bf271bf1bb
Mike Hiretsky <mh@calculate.ru>
2011-08-25 11:57:21 +0400

    Add comment to xinit patch.

--- profiles/patches/x11-apps/xinit-1.3-startDM.sh
+++ profiles/patches/x11-apps/xinit-1.3-startDM.sh
@@ -1,5 +1,9 @@
 
 if [[ ${EBUILD_PHASE} == preinst ]]
 then
+    # when the xinit runs by start-stop-daemon, the console is running start-stop-daemon
+    # begins to perceive the bad typing, adding redirect /dev/null to stdin resolve this
+    # problem
+    # sed adds '</dev/null' to call start-stop-daemon of startDM.sh
     sed -ri 's/(\$\{START_STOP_ARGS\})( \|\|)/\1 <\/dev\/null\2/' ${D}/etc/X11/startDM.sh
 fi
