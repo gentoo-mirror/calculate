712878e2f78d1390ee3319116c81167a7c121ca5
Mike Hiretsky <mh@calculate.ru>
2011-11-07 10:23:03 +0400

    Fix patch for init.d/git-daemon.

--- profiles/patches/dev-vcs/git-1-initd.sh
+++ profiles/patches/dev-vcs/git-1-initd.sh
@@ -1,6 +1,6 @@
 post_src_install()
 {
 	# fix init.d/git-daemon for force stop git-daemon
-	sed -ri 's/(\$\{PIDFILE\})$/\1\n\tpkill -f git-daemon.pid\n\ttrue/' ${D}/etc/init.d/git-daemon
+	sed -ri '/\$\{PIDFILE\}$/ {N;s/(\$\{PIDFILE\})(\n\teend \$)\?/\1\n\tres=$?\n\tpkill -f git-daemon.pid\2res/}' /etc/init.d/git-daemon
 	return 0;
 }
