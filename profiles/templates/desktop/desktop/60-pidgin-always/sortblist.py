491c784d8be3694a9130624629e864efbb3f0671
Блохин Юрий <jb@calculate.ru>
2012-08-15 16:36:59 +0400

    Pidgin groups sorting changed

--- profiles/templates/desktop/desktop/60-pidgin-always/sortblist.py
+++ profiles/templates/desktop/desktop/60-pidgin-always/sortblist.py
@@ -4,21 +4,24 @@
 import os,sys
 from xml.dom import minidom
 
-homedir = os.path.expanduser('~')
-blistfile="/.purple/blist.xml"
-
-#открываем файл списка контактов и получаем элементы групп
-blist=minidom.parse(homedir+blistfile)
-elements=blist.getElementsByTagName("group")
-#сортируем группы
-elements.sort(key=lambda elements:elements.getAttribute('name'))
-#удаляем все группы из исходного xml
-for n in blist.childNodes[0].childNodes[1].getElementsByTagName('group'):
-    blist.childNodes[0].childNodes[1].removeChild(n)
-#добавляем отсортированные группы
-for n in elements:
-    blist.childNodes[0].childNodes[1].appendChild(n)
-#выводим отсортированный список в исходный файл
-blistout=open(homedir+blistfile,"w")
-blistout.write(blist.toxml().encode("utf-8"))
-blistout.close()
+try:
+    homedir = os.path.expanduser('~')
+    blistfile="/.purple/blist.xml"
+    
+    #Buddy list file opening and get groups-list
+    blist=minidom.parse(homedir+blistfile)
+    elements=blist.getElementsByTagName("group")
+    #Groups sorting
+    elements.sort(key=lambda elements:elements.getAttribute('name'))
+    #Deleting groups from source xml
+    for n in blist.childNodes[0].childNodes[1].getElementsByTagName('group'):
+        blist.childNodes[0].childNodes[1].removeChild(n)
+    #Add sorted groups
+    for n in elements:
+        blist.childNodes[0].childNodes[1].appendChild(n)
+    #Write to Buddy list file
+    blistout=open(homedir+blistfile,"w")
+    blistout.write(blist.toxml().encode("utf-8"))
+    blistout.close()
+except Exception as e:
+    sys.stderr.write(str(e))
