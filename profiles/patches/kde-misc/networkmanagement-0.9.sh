052f402b0effc5392328af230325b3efa7261c51
Хирецкий Михаил <mh@calculate.ru>
2013-02-18 14:26:57 +0400

    Fix networkmanagement

--- profiles/patches/kde-misc/networkmanagement-0.9.sh
+++ profiles/patches/kde-misc/networkmanagement-0.9.sh
@@ -1,5 +1,5 @@
 if [[ ${EBUILD_PHASE} == postinst ]]
 then
 	einfo "Fix files containt of net-misc/networkmanager"
-	sed -i '/plasma-applet-networkmanagement.desktop/d' ${EROOT}/var/db/pkg/net-misc/networkmanager-0.9.4.0-r2/CONTENTS	
+	sed -i '/plasma-applet-networkmanagement.desktop/d' ${EROOT}/var/db/pkg/net-misc/networkmanager-0.9*/CONTENTS	
 fi
