2500e8538298354a18855289c66b2b1240e0d8c8
Mike khiretskiy <mh@calculate.ru>
2014-04-01 09:26:20 +0400

    Удалены настройки принудительной установки цвета для ls, grep

--- profiles/templates/3.1/2_ac_install_merge/app-shells/bash/color.sh
+++ profiles/templates/3.1/2_ac_install_merge/app-shells/bash/color.sh
@@ -2,10 +2,6 @@
 #!/bin/bash
 
 if ${use_color} ; then
-	alias ls='ls --color=yes'
-	alias grep='grep --colour=yes'
-	alias egrep='egrep --colour=yes'
-	alias fgrep='fgrep --colour=yes'
 	alias emerge='emerge --color=y'
 	alias eix='eix -F'
 fi
