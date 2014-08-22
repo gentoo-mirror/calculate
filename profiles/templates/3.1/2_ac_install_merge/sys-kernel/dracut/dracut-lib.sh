33043a7e46f3a70a3e6b587015061733f839820a
Mike Khiretskiy <mh@calculate.ru>
2014-08-22 11:54:12 +0400

    Указаны границы версий для патча dracut

--- profiles/templates/3.1/2_ac_install_merge/sys-kernel/dracut/dracut-lib.sh
+++ profiles/templates/3.1/2_ac_install_merge/sys-kernel/dracut/dracut-lib.sh
@@ -1,3 +1,3 @@
-# Calculate dotall format=patch path=/usr/lib/dracut/modules.d/99base
+# Calculate dotall format=patch path=/usr/lib/dracut/modules.d/99base pkg(sys-kernel/dracut)<39
 <reg>(\nwait_for_dev.*?_name.*?str_replace.*)'\\x2f'</reg>
 <text>\1'\\\\x2f'</text>
