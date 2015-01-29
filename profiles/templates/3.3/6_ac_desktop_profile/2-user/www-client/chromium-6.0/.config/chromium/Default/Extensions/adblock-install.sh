a5c93b6c46612cbcf9bc15affddd424bcb3b73b5
Mike Khiretskiy <mh@calculate.ru>
2015-01-29 12:53:22 +0300

    Удаление лишних параметров (path,name) у выполняемых шаблонов (exec,run)

--- profiles/templates/3.3/6_ac_desktop_profile/2-user/www-client/chromium-6.0/.config/chromium/Default/Extensions/adblock-install.sh
+++ profiles/templates/3.3/6_ac_desktop_profile/2-user/www-client/chromium-6.0/.config/chromium/Default/Extensions/adblock-install.sh
@@ -1,4 +1,4 @@
-# Calculate path=/tmp pkg(www-apps/chromium-adblock)!= exec=/bin/bash name=adblock-install-#-ur_login-#.sh
+# Calculate pkg(www-apps/chromium-adblock)!= exec=/bin/bash
 
 cp -r /usr/share/chromium-adblock/gighmmpiobklfepjocnamgkkbiglidom #-ur_home_path-#/.config/chromium/Default/Extensions/
 chown #-ur_login-#:#-ur_group-# -R #-ur_home_path-#/.config/chromium/Default/Extensions/gighmmpiobklfepjocnamgkkbiglidom &>/dev/null
--- profiles/templates/3.3/6_ac_desktop_profile/2-user/www-client/chromium-6.0/.config/chromium/Default/Extensions/ublock-install.sh
+++ profiles/templates/3.3/6_ac_desktop_profile/2-user/www-client/chromium-6.0/.config/chromium/Default/Extensions/ublock-install.sh
@@ -1,4 +1,4 @@
-# Calculate path=/tmp pkg(www-apps/chromium-ublock)!= exec=/bin/bash name=adblock-install-#-ur_login-#.sh
+# Calculate pkg(www-apps/chromium-ublock)!= exec=/bin/bash
 
 cp -r /usr/share/chromium-ublock/cjpalhdlnbpafiamejdnhcphjbkeiagm #-ur_home_path-#/.config/chromium/Default/Extensions/
 chown #-ur_login-#:#-ur_group-# -R #-ur_home_path-#/.config/chromium/Default/Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm &>/dev/null
