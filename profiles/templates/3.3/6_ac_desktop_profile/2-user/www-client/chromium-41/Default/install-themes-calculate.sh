a26f916765e51f0037e5b6b4f11e3aaf05811ced
Alexander Tratsevskiy <at@calculate.ru>
2015-03-17 18:38:14 +0300

    Удален chrome-themes-calculate

--- profiles/templates/3.3/6_ac_desktop_profile/2-user/www-client/chromium-41/Default/install-themes-calculate.sh
+++
@@ -1,6 +0,0 @@
-# Calculate pkg(www-plugins/chrome-themes-calculate)!= exec=/bin/bash
-
-mkdir -p #-ur_home_path-#/.config/chromium/Default/Extensions/clcbojcafbggjenbeflknhfimpcikmlc
-cp -r /usr/share/chrome-themes-calculate/* #-ur_home_path-#/.config/chromium/Default/Extensions/clcbojcafbggjenbeflknhfimpcikmlc
-chown #-ur_login-#:#-ur_group-# -R #-ur_home_path-#/.config/chromium/Default/Extensions
-exit 0
