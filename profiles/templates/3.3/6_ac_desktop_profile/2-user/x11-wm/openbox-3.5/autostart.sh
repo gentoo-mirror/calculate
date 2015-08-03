04975b645e15e49b8edb9c3acf257dae65b03038
Repository QA checks <repo-qa-checks@gentoo.org>
2015-08-03 19:19:28 -0400

    2015-08-03 23:19:27 UTC

--- profiles/templates/3.3/6_ac_desktop_profile/2-user/x11-wm/openbox-3.5/autostart.sh
+++ profiles/templates/3.3/6_ac_desktop_profile/2-user/x11-wm/openbox-3.5/autostart.sh
@@ -2,8 +2,8 @@
 
 # You may place a similar script in $HOME/.config/openbox/autostart.d/*
 # to run user-specific things.
-if ls ~/.config/openbox/autostart.d/* &>/dev/null;then
-	for f in ~/.config/openbox/autostart.d/*;do
+if ls ~/.config/openbox/autostart/* &>/dev/null;then
+	for f in ~/.config/openbox/autostart/*;do
 		[ -d "${f}" ] && continue
 		source ${f}
 	done
