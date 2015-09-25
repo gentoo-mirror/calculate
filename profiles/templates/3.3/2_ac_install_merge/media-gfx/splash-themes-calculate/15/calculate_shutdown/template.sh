48a767013d9b04a3346b9fff7c85cc7fe6342d8b
Mike Khiretskiy <mh@calculate.ru>
2015-09-25 10:43:02 +0300

    Шаблоны для splash themes 15

---
+++ profiles/templates/3.3/2_ac_install_merge/media-gfx/splash-themes-calculate/15/calculate_shutdown/template.sh
@@ -0,0 +1,40 @@
+# Calculate append=skip
+
+for fn in *.cfg;
+do
+    resol=${fn%.cfg}
+    width=$( echo $resol | cut -dx -f1 )
+    height=$( echo $resol | cut -dx -f2 )
+    orig_height=1080
+    k="$((1000 - ( 1000 - ${height}000 / ${orig_height} ) / 25 )) / 1000"
+    boxtop=$(( $height - 1 ))
+    text_x=$(( $width / 2 ))
+    text_y=$(( $height / 2 + 60 * $k ))
+    text_size=$(( 20 * $k ))
+
+cat >$fn <<EOF
+# Calculate comment=#
+
+# background image
+silentpic=images/silent-${resol}.jpg
+
+# Coords of system messages
+text_x=${text_x}
+text_y=${text_y}
+text_align middle middle
+
+# system messages font size
+text_size=${text_size}
+
+# System messages color in format [0x|#]rrggbb, or [0x|#]rrggbbaa
+text_color=0xdddddd
+
+# Path to TTF font, for system messages. Path may be:
+text_font=DroidSans.ttf
+
+#progress bar for silent mode
+box silent inter 0 ${boxtop} 0 ${height} #ffffff
+box silent 0 ${boxtop} ${width} ${height} #ffffff
+
+EOF
+done
