d44060973a4a90b3bc65443d5f8a7d488061bd5d
Хирецкий Михаил <mh@calculate.ru>
2016-02-24 14:07:09 +0300

    Исправлен шаблон для bash и wgetpaste

--- profiles/templates/3.3/2_ac_install_merge/app-shells/bash/bashrc.d/wgetpaste_color.sh
+++ profiles/templates/3.3/2_ac_install_merge/app-shells/bash/bashrc.d/wgetpaste_color.sh
@@ -1,4 +1,8 @@
 # Calculate comment=#
 #!/bin/bash
 
-alias glxgears='vblank_mode=0 glxgears'
+# use color output for pastebin.calculate-linux.org by wgetpaste
+if ${use_color} ; then
+       alias emerge='emerge --color=y'
+       alias eix='eix -F'
+fi
