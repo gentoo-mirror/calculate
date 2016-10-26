90489e627d0f48588dd78d6fee7dca205b9fbdb5
Alexander Tratsevskiy <at@calculate.ru>
2016-10-26 18:38:28 +0300

    Исправлены шаблоны загрузки модулей virtualbox, добавлена выгрузка старой версии модулей

--- profiles/templates/3.4/2_ac_install_merge/app-emulation/virtualbox-modules/loadmodules.sh
+++ profiles/templates/3.4/2_ac_install_merge/app-emulation/virtualbox-modules/loadmodules.sh
@@ -1,2 +1,5 @@
-# Calculate exec=/bin/sh cl_ebuild_phase!=
+# Calculate exec=/bin/sh cl_ebuild_phase!=&&cl_chroot_status==off&&cl_system_boot_set==off
+rmmod vboxnetflt vboxnetadp 2>/dev/null
+rmmod vboxdrv 2>/dev/null
 modprobe -a vboxdrv vboxnetflt vboxnetadp
+true
