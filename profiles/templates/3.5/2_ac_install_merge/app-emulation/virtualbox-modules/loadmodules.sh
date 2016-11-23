2cb89a7782d32e3834e0f44c6abfceded0b67a23
Хирецкий Михаил <mh@calculate.ru>
2016-11-23 09:51:32 +0300

    Исправлена загрузка модулей openrc

--- profiles/templates/3.5/2_ac_install_merge/app-emulation/virtualbox-modules/loadmodules.sh
+++ profiles/templates/3.5/2_ac_install_merge/app-emulation/virtualbox-modules/loadmodules.sh
@@ -1,3 +1,3 @@
 # Calculate exec=/bin/sh cl_ebuild_phase!=&&cl_chroot_status==off&&cl_system_boot_set==off
-modprobe -a vboxdrv vboxnetflt vboxnetadp
+modprobe -a vboxdrv vboxnetflt vboxnetadp vboxpci
 true
