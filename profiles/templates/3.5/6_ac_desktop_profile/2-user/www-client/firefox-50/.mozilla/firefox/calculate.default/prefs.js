83359878eaff66210b4185550cbd8018ca60e69a
Alexander Tratsevskiy <at@calculate.ru>
2016-12-18 23:45:53 +0300

    Обновлены маски, исправлена настройка firefox 50

---
+++ profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-50/.mozilla/firefox/calculate.default/prefs.js
@@ -0,0 +1,12 @@
+#?os_locale_language!=ru#
+user_pref("browser.startup.homepage", "http://start.calculate-linux.org");
+#!os_locale_language#
+user_pref("browser.startup.homepage", "http://start.calculate-linux.ru");
+#os_locale_language#
+user_pref("font.name.monospace.x-cyrillic", "Droid Sans Mono Slashed");
+user_pref("font.name.sans-serif.x-cyrillic", "Droid Sans");
+user_pref("font.name.serif.x-cyrillic", "Times New Roman");
+# close a window with multiple tabs
+user_pref("browser.tabs.warnOnClose", false);
+# skip setting the first start
+user_pref("browser.startup.homepage_override.mstone", "ignore");
