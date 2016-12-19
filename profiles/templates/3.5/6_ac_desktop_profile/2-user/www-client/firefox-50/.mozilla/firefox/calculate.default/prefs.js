1ce384fe63586bf876f4429a9b2d8c95b75d071c
Alexander Tratsevskiy <at@calculate.ru>
2016-12-19 19:14:31 +0300

    Оптимизированы настройки Firefox

--- profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-50/.mozilla/firefox/calculate.default/prefs.js
+++ profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-50/.mozilla/firefox/calculate.default/prefs.js
@@ -2,6 +2,7 @@
 user_pref("browser.startup.homepage", "http://start.calculate-linux.org");
 #!os_locale_language#
 user_pref("browser.startup.homepage", "http://start.calculate-linux.ru");
+user_pref("browser.fixup.alternate.suffix", ".ru");
 #os_locale_language#
 user_pref("font.name.monospace.x-cyrillic", "Droid Sans Mono Slashed");
 user_pref("font.name.sans-serif.x-cyrillic", "Droid Sans");
@@ -10,3 +11,9 @@ user_pref("font.name.serif.x-cyrillic", "Times New Roman");
 user_pref("browser.tabs.warnOnClose", false);
 # skip setting the first start
 user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.cache.use_new_backend", 1);
+user_pref("browser.sessionstore.interval", 60000);
+user_pref("browser.tabs.animate", false);
+user_pref("network.http.pipelining.ssl", true);
+user_pref("network.http.pipelining", true);
+user_pref("network.http.proxy.pipelining", true);
