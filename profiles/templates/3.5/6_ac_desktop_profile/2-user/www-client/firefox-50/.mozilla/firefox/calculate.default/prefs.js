8e1eb098294278dda2a5f9efe35caf9e64497716
Alexander Tratsevskiy <at@calculate.ru>
2016-12-19 22:11:47 +0300

    Обновлены настройки Firefox

--- profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-50/.mozilla/firefox/calculate.default/prefs.js
+++ profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-50/.mozilla/firefox/calculate.default/prefs.js
@@ -11,10 +11,29 @@ user_pref("font.name.serif.x-cyrillic", "Times New Roman");
 user_pref("browser.tabs.warnOnClose", false);
 # skip setting the first start
 user_pref("browser.startup.homepage_override.mstone", "ignore");
+# включение новой системы HTTP кэширования, улучшает отзывчивость интерфейса
 user_pref("browser.cache.use_new_backend", 1);
 user_pref("browser.sessionstore.interval", 60000);
 user_pref("browser.tabs.animate", false);
+# максимальное кол-во запросов в соединении
 user_pref("network.http.pipelining.max-optimistic-requests", 8);
+# включает конвейерные запросы через SSL соединение (https)
 user_pref("network.http.pipelining.ssl", true);
 user_pref("network.http.pipelining", true);
+# использовать несколько запросов в одном соединении с сервером при использовании прокси
 user_pref("network.http.proxy.pipelining", true);
+# максимальное кол-во закэшированных страниц в каждой вкладке
+user_pref("browser.sessionhistory.max_total_viewers", 3);
+# асинхронная инициализация плагинов
+user_pref("dom.ipc.plugins.asyncInit.enabled", true);
+# асинхронный вызов стеков
+user_pref("javascript.options.asyncstack", true);
+user_pref("dom.ipc.plugins.asyncdrawing.enabled", true);
+# высвобождает неиспользуемые страницы памяти после закрытия вкладок
+user_pref("memory.free_dirty_pages", true);
+#?hr_cpu_num>1#
+# многопроцессорный режим
+user_pref("browser.tabs.remote.autostart", true);
+# кол-во используемых процессоров
+user_pref("dom.ipc.processCount", #-hr_cpu_num-#);
+#hr_cpu_num#
