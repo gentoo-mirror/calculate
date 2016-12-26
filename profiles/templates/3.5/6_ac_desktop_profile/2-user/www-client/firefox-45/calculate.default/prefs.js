baecf2b589aa14bda272a50f26cda30d43ef5c5d
Alexander Tratsevskiy <at@calculate.ru>
2016-12-26 12:56:58 +0300

    Обновлены настройки Firefox

--- profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-45/calculate.default/prefs.js
+++ profiles/templates/3.5/6_ac_desktop_profile/2-user/www-client/firefox-45/calculate.default/prefs.js
@@ -72,7 +72,7 @@ user_pref("extensions.pocket.site", "");
 user_pref("font.name.monospace.x-cyrillic", "Droid Sans Mono Slashed");
 user_pref("font.name.sans-serif.x-cyrillic", "Droid Sans");
 user_pref("font.name.serif.x-cyrillic", "Times New Roman");
-user_pref("general.warnonaboutconfig", false);
+user_pref("general.warnOnAboutConfig", false);
 user_pref("geo.enabled", false);
 user_pref("geo.wifi.uri", "");
 user_pref("javascript.options.asyncstack", true);
