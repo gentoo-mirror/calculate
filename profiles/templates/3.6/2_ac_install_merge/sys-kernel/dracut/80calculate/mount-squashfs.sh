9bb1b5c9cff1e6e397d1ff50b9ca062216af6898
Хирецкий Михаил <mh@calculate.ru>
2018-10-03 17:28:32 +0300

    Изменена загрузка livecd с опцией docache
    * добавлено отключение /run/initramfs/isoscan и iso образа при успешном
    копировании squash в память

--- profiles/templates/3.6/2_ac_install_merge/sys-kernel/dracut/80calculate/mount-squashfs.sh
+++ profiles/templates/3.6/2_ac_install_merge/sys-kernel/dracut/80calculate/mount-squashfs.sh
@@ -55,6 +55,8 @@ do_cache() {
             SQUASHED="${LIVECACHE}/livecd.squashfs"
             echo "Unmounting media from /var/initramfs/live"
             umount -n /run/initramfs/live
+            umount -nd /run/initramfs/live
+            [[ -d /run/initramfs/isoscan ]] && umount -nd /run/initramfs/isoscan
         fi
     else
         echo "Too low memory for cache the loop file." 1>&2
