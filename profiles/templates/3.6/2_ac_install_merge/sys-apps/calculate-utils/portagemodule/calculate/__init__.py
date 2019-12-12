dabdba045dee8ee5b2f4232cb3b48711c8338b7a
Хирецкий Михаил <mh@calculate.ru>
2019-12-12 12:56:01 +0300

    Миграция на использование repos.conf вместо layman
    * обновление утилит
    * миграция текущих профилей и расположения репозиториев
    * исправление текущих Calculate утилит для работы с шаблонами,
    расположенными по симлинку

---
+++ profiles/templates/3.6/2_ac_install_merge/sys-apps/calculate-utils/portagemodule/calculate/__init__.py
@@ -0,0 +1,29 @@
+# Copyright 2014-2018 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Calculate plug-in module for portage.
+Performs a git pull on repositories."""
+__doc__ = doc[:]
+
+from portage.localization import _
+from portage.sync.config_checks import CheckSyncConfig
+from portage.util import writemsg_level
+
+
+module_spec = {
+	'name': 'calcualte',
+	'description': doc,
+	'provides':{
+		'calculate-module': {
+			'name': "calculate",
+			'sourcefile': "calculate",
+			'class': "CalculateSync",
+			'description': doc,
+			'functions': ['sync'],
+			'func_desc': {
+				'sync': 'Performs a calculate overlay sychronization',
+			},
+			'validate_config': CheckSyncConfig,
+		}
+	}
+}
