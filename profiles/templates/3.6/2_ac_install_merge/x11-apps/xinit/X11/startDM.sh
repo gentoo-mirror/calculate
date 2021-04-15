e2a9d19a6a8e136d3951c6f778e4048194648a5c
Хирецкий Михаил <mh@calculate.ru>
2021-04-15 09:07:01 +0300

    x11-apps/xinit: восстановлен startDM.sh

--- profiles/templates/3.6/2_ac_install_merge/x11-apps/xinit/X11/startDM.sh
+++ profiles/templates/3.6/2_ac_install_merge/x11-apps/xinit/X11/startDM.sh
@@ -1,3 +1,32 @@
-# Calculate comment=# format=patch chmod=0755 autoupdate
-<reg>\$\{START_STOP_ARGS\} \|\|</reg>
-<text>${START_STOP_ARGS} &lt;/dev/null ||</text>
+# Calculate comment=# chmod=0755 autoupdate
+#!/bin/sh
+# Copyright 1999-2019 Gentoo Authors
+# Distributed under the terms of the GNU General Public License, v2
+
+# We need to source /etc/profile for stuff like $LANG to work
+# bug #10190.
+. /etc/profile
+
+. /lib/rc/sh/functions.sh
+
+# Bail out early if on a non-OpenRC system:
+if [ ! -d /run/openrc ]; then
+	eerror "$0 should only be used on OpenRC systems"
+fi
+
+# baselayout-1 compat
+if ! type get_options >/dev/null 2>/dev/null ; then
+	[ -r "${svclib}"/sh/rc-services.sh ] && . "${svclib}"/sh/rc-services.sh
+fi
+
+export RC_SVCNAME=xdm
+EXEC="$(get_options service)"
+NAME="$(get_options name)"
+PIDFILE="$(get_options pidfile)"
+START_STOP_ARGS="$(get_options start_stop_args)"
+
+start-stop-daemon --start --exec ${EXEC} \
+${NAME:+--name} ${NAME} ${PIDFILE:+--pidfile} ${PIDFILE} ${START_STOP_ARGS} </dev/null || \
+eerror "ERROR: could not start the Display Manager"
+
+# vim:ts=4
