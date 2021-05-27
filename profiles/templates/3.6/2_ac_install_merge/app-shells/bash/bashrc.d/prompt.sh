222a3474331f0015f82113b61da00c00c5fff7b8
Хирецкий Михаил <mh@calculate.ru>
2021-05-27 16:15:34 +0300

    app-shells/bash: цвето root консоли для LXC

--- profiles/templates/3.6/2_ac_install_merge/app-shells/bash/bashrc.d/prompt.sh
+++ profiles/templates/3.6/2_ac_install_merge/app-shells/bash/bashrc.d/prompt.sh
@@ -13,7 +13,11 @@ if ${use_color} ; then
 		PS_HOSTNAME="$(hostname)"
 	fi
 	if [[ ${EUID} == 0 ]] ; then
+#?in(os_linux_pkglist,container)==#
 		PS1='\[\033[01;31m\]${PS_HOSTNAME}\[\033[01;34m\] \W \$\[\033[00m\] '
+#!in#
+		PS1='\[\033[00;33m\]${PS_HOSTNAME}\[\033[01;34m\] \W \$\[\033[00m\] '
+#in#
 	else
 		PS1='\[\033[01;32m\]\u@${PS_HOSTNAME}\[\033[01;34m\] \w \$\[\033[00m\] '
 	fi
