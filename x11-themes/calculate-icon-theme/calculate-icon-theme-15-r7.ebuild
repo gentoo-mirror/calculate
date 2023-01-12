gentoo-mirror/calculate

commit 86eb4f95597173efb5c695533446167fb866804d

x11-themes/elementary-icon-theme: fix gnome2 -> xdg-utils

x11-themes/elementary-icon-theme/elementary-icon-theme-2.7.1-r4.ebuild
@@ -1,10 +1,10 @@
-# Copyright 1999-2014 Gentoo Foundation
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 # $Header: $
 
-EAPI=7
+EAPI=8
 
-inherit gnome2
+inherit xdg-utils
 
 DESCRIPTION="Elementary icon theme is designed to be smooth, sexy, clear, and efficient"
 HOMEPAGE="https://launchpad.net/elementaryicons"
@@ -16,9 +16,7 @@ LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="amd64 x86"
 
-DEPEND=""
-RDEPEND="${DEPEND}
-		x11-themes/hicolor-icon-theme"
+RDEPEND="x11-themes/hicolor-icon-theme"
 
 S="${WORKDIR}"
 
@@ -28,13 +26,13 @@ src_install() {
 }
 
 pkg_preinst() {
-	gnome2_icon_savelist
+	xdg_icon_savelist
 }
 
 pkg_postinst() {
-	gnome2_icon_cache_update
+	xdg_icon_cache_update
 }
 
 pkg_postrm() {
-	gnome2_icon_cache_update
+	xdg_icon_cache_update
 }

x11-themes/elementary-icon-theme/elementary-icon-theme-2.7.1-r5.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
-inherit gnome2
+inherit xdg-utils
 
 DESCRIPTION="Elementary icon theme is designed to be smooth, sexy, clear, and efficient"
 HOMEPAGE="https://launchpad.net/elementaryicons"
@@ -15,9 +15,7 @@ LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="amd64 x86"
 
-DEPEND=""
-RDEPEND="${DEPEND}
-		x11-themes/hicolor-icon-theme"
+RDEPEND="x11-themes/hicolor-icon-theme"
 
 S="${WORKDIR}"
 
@@ -27,13 +25,13 @@ src_install() {
 }
 
 pkg_preinst() {
-	gnome2_icon_savelist
+	xdg_icon_savelist
 }
 
 pkg_postinst() {
-	gnome2_icon_cache_update
+	xdg_icon_cache_update
 }
 
 pkg_postrm() {
-	gnome2_icon_cache_update
+	xdg_icon_cache_update
 }

commit 4560d5b7102163a8ff905009ccd75e4766a749bc

x11-themes/*: remove call to an non-existant `xdg_icon_savelist` function
The issue has been introduced by 86eb4f95597173efb5c695533446167fb866804d .

Signed-off-by: Alexander Kurakin <kuraga333@mail.ru>

x11-themes/calculate-icon-theme/calculate-icon-theme-15-r7.ebuild
@@ -32,10 +32,6 @@ src_install() {
 	doins -r .
 }
 
-pkg_preinst() {
-	xdg_icon_savelist
-}
-
 pkg_postinst() {
 	xdg_icon_cache_update
 }

x11-themes/calculate-icon-theme/calculate-icon-theme-15-r8.ebuild
@@ -32,10 +32,6 @@ src_install() {
 	doins -r .
 }
 
-pkg_preinst() {
-	xdg_icon_savelist
-}
-
 pkg_postinst() {
 	xdg_icon_cache_update
 }

x11-themes/calculate-icon-theme/calculate-icon-theme-15-r9.ebuild
@@ -33,10 +33,6 @@ src_install() {
 	doins -r .
 }
 
-pkg_preinst() {
-	xdg_icon_savelist
-}
-
 pkg_postinst() {
 	xdg_icon_cache_update
 }

x11-themes/calculate-icon-theme/calculate-icon-theme-19.ebuild
@@ -33,10 +33,6 @@ src_install() {
 	doins -r .
 }
 
-pkg_preinst() {
-	xdg_icon_savelist
-}
-
 pkg_postinst() {
 	xdg_icon_cache_update
 }

x11-themes/elementary-icon-theme/elementary-icon-theme-2.7.1-r4.ebuild
@@ -25,10 +25,6 @@ src_install() {
 	doins -r elementary
 }
 
-pkg_preinst() {
-	xdg_icon_savelist
-}
-
 pkg_postinst() {
 	xdg_icon_cache_update
 }

x11-themes/elementary-icon-theme/elementary-icon-theme-2.7.1-r5.ebuild
@@ -24,10 +24,6 @@ src_install() {
 	doins -r elementary
 }
 
-pkg_preinst() {
-	xdg_icon_savelist
-}
-
 pkg_postinst() {
 	xdg_icon_cache_update
 }