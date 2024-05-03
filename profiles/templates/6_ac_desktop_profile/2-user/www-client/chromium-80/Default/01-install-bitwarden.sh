c32950594101b53ae8085856378f8ce96649175f
Павел Иванов <pv@calculate.ru>
2024-05-03 15:51:14 +0300

    TG-580 fix templates:
    * Обновлены шаблоны настройки плагина bitwarden

--- profiles/templates/6_ac_desktop_profile/2-user/www-client/chromium-80/Default/01-install-bitwarden.sh
+++ profiles/templates/6_ac_desktop_profile/2-user/www-client/chromium-80/Default/01-install-bitwarden.sh
@@ -5,68 +5,59 @@ mkdir -p #-ur_home_path-#/.config/chromium/Default/Extensions/${plugin_id}
 mkdir -p "#-ur_home_path-#/.config/chromium/Default/Local Extension Settings" &>/dev/null
 cp -r /usr/share/chrome-bitwarden/* #-ur_home_path-#/.config/chromium/Default/Extensions/$plugin_id
 
-/usr/share/chrome-bitwarden/configure.py #-ur_home_path-#/.config/chromium $plugin_id "#-ini(user_credentials.bitwarden_login)-#" "#-ini(user_credentials.bitwarden_pw)-#" "#-ini(user_credentials.bitwarden_url)-#" &>/dev/null <<EOF
+/usr/share/chrome-bitwarden/configure.py #-ur_home_path-#/.config/chromium $plugin_id "#-ini(user_credentials.bitwarden_login)-#" "#-ini(user_credentials.bitwarden_pw)-#" "#-ini(user_credentials.bitwarden_url)-#" <<EOF
 {
     "userId": {
+	"ciphers": {},
 	"data": {
 	    "ciphers": {
 		"encrypted": {}
 	    },
-	    "collections": {
-		"encrypted": {}
-	    },
-	    "folders": {
+	    "passwordGenerationHistory": {},
+	    "sends": {
 		"encrypted": {}
 	    }
 	},
 	"keys": {
-	    "cryptoSymmetricKey": {
-		"encrypted": {}
-	    },
-	    "organizationKeys": {
-		"encrypted": {}
-	    },
-	    "privateKey": {
-		"encrypted": {}
-	    },
-	    "providerKeys": {
-		"encrypted": {}
-	    }
+	    "cryptoSymmetricKey": {},
+	    "masterKeyEncryptedUserKey": {}
 	},
 	"profile": {
+	    "email": "#-ini(user_credentials.bitwarden_login)-#",
+	    "emailVerified": true,
+	    "keyHash": {},
 	    "userId": {}
 	},
 	"settings": {
-	    "avatarColor": null,
-	    "defaultUriMatch": 1,
-	    "enableAutoFillOnPageLoad": true,
-	    "environmentUrls": {
-		"api": null,
-		"base": {},
-		"events": null,
-		"icons": null,
-		"identity": null,
-		"keyConnector": null,
-		"notifications": null,
-		"webVault": null
-	    },
 	    "vaultTimeout": null,
 	    "vaultTimeoutAction": "lock"
 	},
-	"tokens": {
-	    "accessToken": {},
-	    "refreshToken": {}
-	}
+	"tokens": {}
     },
-    "userId_masterkey_auto": {},
+    "userId_user_auto": {},
     "activeUserId": {},
     "authenticatedAccounts": {},
-    "global": {
-	"stateVersion":7,
-	"theme":"system"
-    },
     "user_userId_autofillSettings_autofillOnPageLoad": true,
-    "user_userId_autofillSettings_autofillOnPageLoadDefault": true
+    "user_userId_autofillSettings_autofillOnPageLoadDefault": true,
+    "user_userId_autofillSettings_autofillOnPageLoadPolicyToastHasDisplayed": false,
+    "user_userId_crypto_organizationKeys": {
+	"organizationId": {
+	    "key": {},
+	    "type": "organization"
+	}
+    },
+    "user_userId_crypto_privateKey": {},
+    "user_userId_domainSettings_defaultUriMatchStrategy": 1,
+    "user_userId_environment_environment": {
+	"urls": {
+	    "base": "#-ini(user_credentials.bitwarden_url)-#"
+	}
+    },
+    "user_userId_token_accessToken": {},
+    "user_userId_token_refreshToken": {},
+    "user_userId_userDecryptionOptions_decryptionOptions": {
+	"hasMasterPassword": true
+    }
 }
 EOF
 
