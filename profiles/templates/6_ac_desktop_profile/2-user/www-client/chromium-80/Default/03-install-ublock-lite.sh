fc131e2f27665e2028edc5a60d869b44a51bc740
root <root@pc283.spb.calculate.ru>
2025-03-06 18:27:50 +0300

    ebuild and install ublock lite

---
+++ profiles/templates/6_ac_desktop_profile/2-user/www-client/chromium-80/Default/03-install-ublock-lite.sh
@@ -0,0 +1,12 @@
+# Calculate pkg(www-plugins/chrome-ublock-lite)!= run=/bin/bash
+
+plugin_id="ddkjiahejlhfcafbddmgiahcphecmpfh"
+mkdir -p #-ur_home_path-#/.config/chromium/Default/Extensions/$plugin_id
+mkdir -p "#-ur_home_path-#/.config/chromium/Default/Local Extension Settings" &>/dev/null
+cp -r /usr/share/chrome-ublock-lite/* #-ur_home_path-#/.config/chromium/Default/Extensions/$plugin_id
+
+/usr/share/chrome-ublock/configure.py #-ur_home_path-#/.config/chromium $plugin_id &>/dev/null 
+
+chown #-ur_login-#:#-ur_group-# -R #-ur_home_path-#/.config/chromium/Default/Extensions
+chown #-ur_login-#:#-ur_group-# -R "#-ur_home_path-#/.config/chromium/Default/Local Extension Settings"
+exit 0
