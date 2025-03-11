8dea2b9086d689deb0609c280e0af10e679a7507
root <root@pc283.spb.calculate.ru>
2025-03-11 17:42:37 +0300

    fix for ublock-lite

--- profiles/templates/6_ac_desktop_profile/2-user/www-client/chromium-80/Default/03-install-ublock-lite.sh
+++ profiles/templates/6_ac_desktop_profile/2-user/www-client/chromium-80/Default/03-install-ublock-lite.sh
@@ -5,7 +5,7 @@ mkdir -p #-ur_home_path-#/.config/chromium/Default/Extensions/$plugin_id
 mkdir -p "#-ur_home_path-#/.config/chromium/Default/Local Extension Settings" &>/dev/null
 cp -r /usr/share/chrome-ublock-lite/* #-ur_home_path-#/.config/chromium/Default/Extensions/$plugin_id
 
-/usr/share/chrome-ublock/configure.py #-ur_home_path-#/.config/chromium $plugin_id &>/dev/null 
+/usr/share/chrome-ublock-lite/configure.py #-ur_home_path-#/.config/chromium $plugin_id &>/dev/null 
 
 chown #-ur_login-#:#-ur_group-# -R #-ur_home_path-#/.config/chromium/Default/Extensions
 chown #-ur_login-#:#-ur_group-# -R "#-ur_home_path-#/.config/chromium/Default/Local Extension Settings"
